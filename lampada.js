307cc896f8b6e4a1615eeccc83e851b7e39e53c0
Max Soares <118302093+maxsoares13@users.noreply.github.com>
2022-12-10 14:39:03 -0300

    Desafio da Lampada

---
+++ lampada.js
@@ -0,0 +1,21 @@
+var lamp = window.document.getElementById('lampada')
+
+
+function estaQuebrada() {
+    return lamp.src.indexOf("quebrada") > - 1;
+}
+
+function Ligar(){
+    if (!estaQuebrada()){
+    lamp.src = "ligada.svg"}
+}
+
+function desligar(){
+    if (!estaQuebrada()){
+    lamp.src = "desligada.svg"}
+}
+
+lamp.addEventListener("click", quebrar)
+function quebrar(){
+    lamp.src = "quebrada.svg"
+}
